d4a3c8efb70abb11a5911e771c6eb0a1be648591
ZoeMohamed <zoemohamed31@gmail.com>
2022-09-06 14:57:57 +0700

    publish api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -43,65 +43,138 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+
     public function store(Request $request)
     {
-
+        $request->validate(
+            [
+                "foto" => "required|file|max:3072",
+                "nama" => "required",
+                "kode" => "required",
+                "unit" => "required",
+                "deskripsi" => "required",
+                "switch_pembelian" => "required",
+                "switch_penjualan" => "required",
+            ]
+        );
+        $product = new Product();
+        $filename = time() . '.jpg';
+        $product->foto = $filename;
         if ($request->hasFile('foto')) {
-            // $request->validate(
-            //     [
-            //         "foto" => "file|max:3072",
-            //         "nama" => "required",
-            //         "kode" => "required",
-            //         "unit" => "required",
-            //         "deskripsi" => "required",
-            //         "switch_pembelian" => "required",
-            //         "switch_pejualan" => "required",
-            //     ]
-            // );
-
-            // $product = new Product();
-            $image  = $request->file('foto');
-            // $image_name = $image->getClientOriginalName();
-            $input = $image->store('products');
-
-            Product::create([
-                "foto" => $input,
-                "nama" => $request->nama,
-                "unit" => $request->unit,
-                "kode" => $request->kode,
-                "deskripsi" => $request->deskripsi,
-                "switch_pembelian" => $request->switch_pembelian,
-                "switch_penjualan" => $request->switch_penjualan
-            ]);
-            // $input['foto'] = $image_name;
-
-            // $product->foto = $image_name;
-
-            // Product::create($request->all());
+            $request->file('foto')->storeAs('/galeri', $filename);
         }
-        // Product::create($request->all());
-
-
-
-
-
-        // // $result = $request->file('foto')->store('storage/app/public');
-
-        // // $request['foto'] = $result;
-
-
-        // $input = $request->all();
-        // Product::create($input);
-
-        // // dd($input);
-        // if ($request->hasFile('foto')) {
-        //     $image  = $request->file('foto');
-        //     $image_name = $image->getClientOriginalName();
-        //     $input = $request->file('foto')->storeAs('/public', $image_name);
-        //     $input['foto'] = $image_name;
-        //     // Product::create($input);
-        // }
+        $product->nama = $request->nama;
+        $product->kode = $request->kode;
+        $product->unit = $request->unit;
+        $product->deskripsi = $request->deskripsi;
+        $product->switch_pembelian = $request->switch_pembelian;
+        $product->switch_penjualan = $request->switch_penjualan;
+        $tes = $product->save();
+ 
+        return response()->json([
+            'data' => $product
+        ]);
     }
+    // public function store(Request $request)
+    // {
+    //     $request->validate([
+    //         "foto" => "required|file|max:3072",
+    //         "nama" => "required",
+    //         "kode" => "required",
+    //         "unit" => "required",
+    //         "deskripsi" => "required",
+    //         "switch_pembelian" => "required",
+    //         "switch_pejualan" => "required",
+    //     ]);
+    //     $product = new Product();
+    //     $filename = time(). '.jpg';
+
+    //     $product->foto = $filename;
+    //     if($request->hasFile('foto')){
+    //         $filename = $request->file('foto')->storeAs('/foto');
+    //     }
+    //     $product->nama = $request->nama;
+    //     $product->kode = $request->kode;
+    //     $product->unit = $request->unit;
+    //     $product->deskripsi = $request->deskripsi;
+    //     $product->switch_pembelian = $request->switch_pembelian;
+    //     $product->switch_penjualan = $request->switch_penjualan;
+    //     $result = $product->save();
+
+    //     if($result){
+    //         return response()->json(['status' => true]);
+    //     }else{
+    //         return response()->json(['status' => false]);
+
+    //     }
+
+    //     // Product::create([
+    //     //     "foto" => $request->foto,
+    //     //     "nama" => $request->nama,
+    //     //     "unit" => $request->unit,
+    //     //     "kode" => $request->kode,
+    //     //     "deskripsi" => $request->deskripsi,
+    //     //     "switch_pembelian" => $request->switch_pembelian,
+    //     //     "switch_penjualan" => $request->switch_penjualan
+    //     // ]);
+
+    //     // if ($request->hasFile('foto')) {
+    //     //     // $request->validate(
+    //     //     //     [
+    //     //     //         "foto" => "file|max:3072",
+    //     //     //         "nama" => "required",
+    //     //     //         "kode" => "required",
+    //     //     //         "unit" => "required",
+    //     //     //         "deskripsi" => "required",
+    //     //     //         "switch_pembelian" => "required",
+    //     //     //         "switch_pejualan" => "required",
+    //     //     //     ]
+    //     //     // );
+
+    //     //     // $product = new Product();
+    //     //     $image  = $request->file('foto');
+    //     //     // $image_name = $image->getClientOriginalName();
+    //     //     $input = $image->store('products');
+
+    //     //     Product::create([
+    //     //         "foto" => $input,
+    //     //         "nama" => $request->nama,
+    //     //         "unit" => $request->unit,
+    //     //         "kode" => $request->kode,
+    //     //         "deskripsi" => $request->deskripsi,
+    //     //         "switch_pembelian" => $request->switch_pembelian,
+    //     //         "switch_penjualan" => $request->switch_penjualan
+    //     //     ]);
+    //     //     // $input['foto'] = $image_name;
+
+    //     //     // $product->foto = $image_name;
+
+    //     //     // Product::create($request->all());
+        
+    //     // Product::create($request->all());
+
+
+
+
+
+    //     // // $result = $request->file('foto')->store('storage/app/public');
+
+    //     // // $request['foto'] = $result;
+
+
+    //     // $input = $request->all();
+    //     // Product::create($input);
+
+    //     // // dd($input);
+    //     // if ($request->hasFile('foto')) {
+    //     //     $image  = $request->file('foto');
+    //     //     $image_name = $image->getClientOriginalName();
+    //     //     $input = $request->file('foto')->storeAs('/public', $image_name);
+    //     //     $input['foto'] = $image_name;
+    //     //     // Product::create($input);
+    //     // }
+    // }
 
     /**
      * Display the specified resource.
--- database/migrations/2022_09_05_153607_create_products_table.php
+++ database/migrations/2022_09_05_153607_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string("foto");
+            $table->string("foto")->nullable();
             $table->string('nama');
             $table->string('kode');
             $table->string('unit');
